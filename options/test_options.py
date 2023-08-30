f8437cbb91aadb15e4cbff24a8f9b83ef5c7ead0
cngvng <cngvng123@gmail.com>
2023-08-30 23:31:45 +0900

    update param for testing using

--- options/test_options.py
+++ options/test_options.py
@@ -4,7 +4,8 @@ class TestOptions(BaseOptions):
     def initialize(self):
         BaseOptions.initialize(self)
 
-        self.parser.add_argument('--warp_checkpoint', type=str, default='checkpoints/gp-vton_partflow_vitonhd_usepreservemask_lrarms_1027/PBAFN_warp_epoch_121.pth', help='load the pretrained model from the specified location')
-        self.parser.add_argument('--gen_checkpoint', type=str, default='checkpoints/gp-vton_gen_vitonhd_wskin_wgan_lrarms_1029/PBAFN_gen_epoch_201.pth', help='load the pretrained model from the specified location')
-        # self.parser.add_argument('--image_pairs_txt', type=str, default='dataset/VITON-HD/test_pairs_paired_1018.txt')
+        self.parser.add_argument('--warp_checkpoint', type=str, default='checkpoints/aug/PFAFN_warp_epoch_101.pth', help='load the pretrained model from the specified location')
+        self.parser.add_argument('--gen_checkpoint', type=str, default='checkpoints/aug/PFAFN_gen_epoch_101.pth', help='load the pretrained model from the specified location')
+        self.parser.add_argument('--phase', type=str, default='test', help='train, val, test, etc')
+
         self.isTrain = False
